25f91e0fc106a21c931baf41f59564d2d8071d19
Fernando Salom <fdosalom@gmail.com>
2023-02-26 10:55:06 +0100

    Update information about this exercise

--- solve/optionals/optionals.playground/Pages/usuarios.xcplaygroundpage/Contents.swift
+++ solve/optionals/optionals.playground/Pages/usuarios.xcplaygroundpage/Contents.swift
@@ -22,18 +22,32 @@ let child2 = User(name: "Nancy", age: 19)
 let users = [
     child1,
     child2,
+    User(name: "Marc", age: 23, childs: [child1]),
     User(name: "John", age: 46, childs: [child1, child2], disability: disability1, income: 50000.0),
+    User(name: "Charles", age: 35, disability: disability1, income: 42000.0),
     User(name: "Mary", age: 28, childs: [child1], income: 20000.0)
 ]
 
 // - - - - - - - - - - - - - - - - -
 // Vamos a crear una comprobación de lo que le va a
 // cobrar Hacienda a cada usuario. Las reglas son:
+// - Si gana entre 0 y 10000 no paga nada
+// - Si gana entre 10000 y 25000 paga 20%
+// - Si gana entre 25000 y 40000 paga 30%
+// - Si gana entre 40000 y 60000 paga 35%
+// - Si tiene hijos paga un 5% menos por cada hijo
+// - Si tiene una discapacidad paga un 10% menos
+// - No puede haber porcentaje negativo por lo que si es así no se cobra nada
+
+// Cuando calcules lo que tiene que pagar cada usuario printea el resultado
 // - - - - - - - - - - - - - - - - -
 
 func comprobacionDeHacienda(para usuario: User) {
 
+}
 
+for user in users {
+    comprobacionDeHacienda(para: user)
 }
 
 //: [Next](@next)
