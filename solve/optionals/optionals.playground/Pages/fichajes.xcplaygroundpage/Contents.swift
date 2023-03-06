5eaa4efc51cbdf4ff27e3da99579beb16c8ef54e
Fernando Salom <fer@rudo.es>
2023-03-06 11:52:31 +0100

    Add exercise for transfer players

--- solve/optionals/optionals.playground/Pages/fichajes.xcplaygroundpage/Contents.swift
+++ solve/optionals/optionals.playground/Pages/fichajes.xcplaygroundpage/Contents.swift
@@ -16,12 +16,25 @@ class Player {
     var name: String?
     var value: Float?
     var clause: Float?
+    var team: Team?
+
+    init(name: String? = nil, value: Float? = nil, clause: Float? = nil, team: Team? = nil) {
+        self.name = name
+        self.value = value
+        self.clause = clause
+        self.team = team
+    }
 }
 
-class Transfers {
+class Transfer {
     var player: Player?
-    var fromTeam: Team?
-    var toTeam: Team?
+    var to: Team?
+    var years: Int?
+    init(player: Player? = nil, to: Team? = nil, years: Int? = nil) {
+        self.player = player
+        self.to = to
+        self.years = years
+    }
 }
 
 let valencia = Team(name: "Valencia")
@@ -29,6 +42,29 @@ let madrid = Team(name: "Madrid", budget: 100.0)
 let manchester = Team(name: "Manchester", budget: 120.0)
 let liverpool = Team(name: "Liverpool")
 
+let valverde = Player(name: "Valverde", value: 5.0, clause: 50.0, team: madrid)
+let salah = Player(name: "Salah", value: 10.0, clause: 30.0, team: liverpool)
 
-var transfers = []
+var transfers = [
+    Transfer(player: valverde, to: liverpool, years: 4),
+Transfer(player: salah, to: manchester)
+]
+
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+// Crea una función que procese todos los fichajes con las siguiente reglas
+// - Al transferir un jugador el equipo al que pertenece
+//   debe de tener presupuesto suficiente lo que es asumir su clausula y su sueldo por año
+// - Si el jugador no tiene años de contrato es que esta gratis esa temporada
+// - Un equipo sin presupuesto solo puede aceptar jugadores gratis
+
+// imprime por pantalla que transferencias se han podido ejecutar y porque
+// (presupuesto suficiente/insuficiente, gratis...)
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+
+func process(these: [Transfer]) {
+
+}
+
+process(these: transfers)
+
 //: [Next](@next)
