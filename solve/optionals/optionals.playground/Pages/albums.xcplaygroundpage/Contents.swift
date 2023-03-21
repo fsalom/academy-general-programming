2aa14450cb4203e2ab668692efe98f06b0dc896d
Fernando Salom <fer@rudo.es>
2023-03-21 13:33:04 +0100

    Add new optional exercise

---
+++ solve/optionals/optionals.playground/Pages/albums.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/*
+ Crea una función que devuelve el titulo del album de un determinado año
+ El usuario puede meter cualquier año
+
+ Infinite" - 12 de noviembre de 1996
+ "The Slim Shady LP" - 23 de febrero de 1999
+ "The Marshall Mathers LP" - 23 de mayo de 2000
+ "The Eminem Show" - 26 de mayo de 2002
+ "Encore" - 12 de noviembre de 2004
+ "Relapse" - 15 de mayo de 2009
+ "Recovery" - 18 de junio de 2010
+ "The Marshall Mathers LP 2" - 5 de noviembre de 2013
+ "Revival" - 15 de diciembre de 2017
+ "Kamikaze" - 31 de agosto de 2018
+ "Music to Be Murdered By" - 17 de enero de 2020
+*/
+
+
+
